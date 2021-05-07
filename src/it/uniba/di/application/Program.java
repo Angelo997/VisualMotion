5e10b1b0162317ec8652c12e704c4cffb017fe40
Angelo Ignomeriello <angelo.ignomeriello@2014@outlook.it>
2021-05-07 14:06:06 +0200

    commentata la funzione showOut

--- src/it/uniba/di/application/Program.java
+++ src/it/uniba/di/application/Program.java
@@ -410,12 +410,10 @@ public class Program extends Utility {
 											Gs.loadLink(connectivityMatrix);
 											Gs.loadConnection(success_ca,fail_ca);
 											Gs.repaint();
-											AODVParser.showOut(simulationDir + "\\logs\\out.txt");
-											/*
-											Gs.color_ca(Color.RED);
-											Gs.loadConnection(fail_ca);
-											Gs.repaint();
-											*/
+											/*AODVParser.showOut(simulationDir + "\\logs\\out.txt");
+											 * mostra per 30 secondi il file out.txt su un JFrame
+											 */
+										
 											
 										}
 										debug("session: " + session + " - move: " + moveCounter + " - isSimulationOk: "
